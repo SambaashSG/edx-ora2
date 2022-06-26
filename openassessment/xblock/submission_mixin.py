4fd33acba6fd7369bc2ec30eff3a53a91e4bf588
kunj08 <kunj.yadav@sambaash.com>
2022-06-26 12:39:09 +0530

    poup html

--- openassessment/xblock/submission_mixin.py
+++ openassessment/xblock/submission_mixin.py
@@ -998,7 +998,6 @@ class SubmissionMixin:
                 if is_point_submitted:
                     context["gamification_point_submitted"] = is_point_submitted
                     context["gamification_point_html"] = gamification_resp.get("popup_html")
-                    context["gamification_resp"] = gamification_resp
             except Exception as e:
                 logger.error(f"GAMIFICATION ERROR: {e}")
             path = 'openassessmentblock/response/oa_response_submitted.html'
